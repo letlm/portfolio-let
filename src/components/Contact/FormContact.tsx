a1046f98a18db594b153290569f2ed48ad37fee5
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-09-01 19:56:21 -0300

    feature: implementação do formulário para contato

--- src/components/Contact/FormContact.tsx
+++ src/components/Contact/FormContact.tsx
@@ -1,12 +1,38 @@
 import { ContainerForm, Input, Message } from './styles';
 
+import emailjs from 'emailjs-com';
+
 function FormContact() {
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_gmail',
+        'template_mxdj0tf',
+        e.target,
+        'R9Tyzm7cdUt74hU3H',
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+
+    e.target.reset();
+  };
+
   return (
-    <ContainerForm>
-      <Input placeholder="Nome" required />
-      <Input placeholder="E-mail" type="email" required />
-      <Message placeholder="Mensagem" required />
-      <button type="submit">Enviar mensagem</button>
+    <ContainerForm onSubmit={sendEmail}>
+      <Input placeholder="Seu nome" required name="from_name" />
+      <Input placeholder="Seu e-mail" type="email" required name="user_email" />
+      <Message placeholder="Mensagem" required name="message" />
+      <button type="submit" value="Submit">
+        Enviar mensagem
+      </button>
     </ContainerForm>
   );
 }
